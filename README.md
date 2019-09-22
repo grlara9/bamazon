68da330b1fc79abedf60228b6efb0565511e9ef4
Guillermo Lara <grlara9@gmail.com>
2019-09-21 17:51:53 -0700

    updated bamazonCustomer js file, fix variable user_quantity added parse int to converted to number

--- README.md
+++ README.md
@@ -1,1 +1,70 @@
 # bamazon
+
+Overview
+
+in this activity, you'll be creating an Amazon-like storefront with the MySQL skills you learned this unit. The app will take in orders from customers and deplete stock from the store's inventory. As a bonus task, you can program your app to track product sales across your store's departments and then provide a summary of the highest-grossing departments in the store.
+
+Clone repo.
+Run command in Terminal or Gitbash 'npm install'
+Run command depending which mode you would like to be on:
+Customer - 'npm run customer'
+Exective - 'npm run exective'
+Run 'ctrl + c' to exit each mode
+What Each JavaScript Does
+BamazonCustomer.js
+
+Prints the products in the store.
+
+Prompts customer which product they would like to purchase by ID number.
+
+Asks for the quantity.
+
+If there is a sufficient amount of the product in stock, it will return the total for that purchase.
+However, if there is not enough of the product in stock, it will tell the user that there isn't enough of the product.
+If the purchase goes through, it updates the stock quantity to reflect the purchase.
+It will also update the product sales in the department table.
+BamazonManager.js
+
+Starts with a menu:
+
+View Products for Sale
+
+End Session
+
+If the manager selects View Products for Sale, it lists all of the products in the store including all of their details.
+
+If the manager selects View Low Inventory, it'll list all the products with less than five items in its StockQuantity column.
+
+If the manager selects Add to Inventory, it allows the manager to select a product and add inventory.
+
+If the manager selects End Session, it ends the session and doesn't go back to the menu.
+
+BamazonExecutive.js
+
+Starts with a menu:
+
+End Session
+If the manager selects View Product Sales by Department, it lists the Department Sales and calculates the total sales from the overhead cost and product sales.
+
+If the manager selects Create New Department, it allows the manager to create a new department and input current overhead costs and product sales. If there are none, by default it will set at 0.
+
+If the manager selects End Session, it ends the session and doesn't go back to the menu.
+
+![alt text](http://url/to/.png)
+![picture](img/mysql.png)
+
+Technologies used
+
+Node.js
+Inquire NPM Package 
+MYSQL NPM Package 
+
+Prerequisites
+- Node.js - Download the latest version of Node https://nodejs.org/en/
+- Create a MYSQL database called 'Bamazon', reference schema.sql
+Built With
+Sublime Text - Text Editor
+MySQLWorkbench
+Terminal/Gitbash
+Author
+Guillermo Lara
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -4,26 +4,17 @@ var available_stock =0;
 
 var connection = mysql.createConnection({
     host: "localhost",
-
     port: 3306,
-
     user: "root",
-
     password: "Gg30358240!",
-
     database: "bamazon"
 });
 
 connection.connect(function(err){
-
     if (err) throw err;
-
     console.log("Connected");
-
-    display_items();
+    start_promise();
 })
-
-
 
 function display_items(){
 
@@ -37,76 +28,71 @@ function display_items(){
         }
         
     })
-    start_promise();
 }
 //The app should then prompt users with two messages.
 //* The first should ask them the ID of the product they would like to buy.
 //* The second message should ask how many units of the product they would like to buy.
 function start_promise(){
+    display_items();
     inquirer.prompt([
         {
-        name: "buy",
+        name: "id",
         type: "input",
         message: "Which product you want to buy? [Choose by ID]",
         validate: function(value){
             if(isNaN(value) === false){
                 return true;
+            }
+            return false;
+            }
+        },
+        {
+        name: "quantity",
+        type: "input",
+        message: "How many units of the product they would like to buy?",
+        validate: function(value){
+            if(isNaN(value) === false){
+                return true;
+            }
+            return false;
+            }   
         }
-        return false;
-    }
-},
-    {
-    name: "quantity",
-    type: "input",
-    message: "How many units of the product they would like to buy?",
-    validate: function(value){
-        if(isNaN(value) === false){
-           
-            return true;
-        }
-        return false;
-    }
-}
 ]).then(function(answer){
+        var user_id = answer.id;
+        var user_quantity = parseInt(answer.quantity);
 
+        connection.query("SELECT * FROM products WHERE id = ?", [user_id],function(err, response){
+        if(err) throw err;
 
-    connection.query("SELECT * FROM `products` WHERE `id` = ?", {id: answer.buy}, function(err, response){
-        if (err) throw err;
         var available_stock = response[0].stock_quantity;
-
-        var user_input = answer.quantity;
         
-        if(available_stock >=  user_input){
+        if(available_stock >=  user_quantity){
 
-            var stock_left = available_stock - user_input;
-            connection.query(
-                "UPDATE products SET ? WHERE ?",
-                [
-                    {
-                        stock_quantity: stock_left
-                    },
-                    {
-                        id: answer.buy
-                    }
-                ],
-                function (err) {
-                    //console.log(price, amountBought);
-                        if (err) throw err;
-                            console.log("==============================================");
-                            console.log("\n\r");
-                            console.log( response[0].stock_quantity + " Items left in stock");
-                            console.log("\n\r");
-                            console.log("Thank you for shopping");
-                            console.log("==============================================");
-                                }
-                );
-                    } else {
-                            console.log("Not enough available, please choose a different quantity");
-                            console.log("\n\r");
-                            console.log("==============================================");
-                           }
-                        })
-                    })
-                }                  
-                
-
+           var stock_left = available_stock - user_quantity;
+           
+           connection.query("UPDATE products SET ? WHERE ?", 
+           [
+               {
+                   stock_quantity:stock_left,
+                },
+            {
+                id: user_id
+            }
+        ], function(err){
+            if (err) throw err;
+            console.log("==============================================");
+            console.log("\n\r");
+            console.log( response[0].stock_quantity + " Items left in stock");
+            console.log("\n\r");
+            console.log("Thank you for shopping");
+            console.log("==============================================");
+              });
+            }
+        else{
+            console.log("Not enough available, please choose a different quantity");
+            console.log("\n\r");
+            console.log("==============================================");
+            }
+        })
+    })
+}
